db22a135bdd33673c7a8a1fbc5e38f7858a20eff
c0smoz <alec.picard@ensea.fr>
2026-04-07 11:44:52 +0200

    séance 4 2/2

--- cafeteria/settings.py
+++ cafeteria/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_cas_ng',
     'cafeteria_app',
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- cafeteria_app/admin.py
+++ cafeteria_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import *
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(User)
 admin.site.register(Product)
 admin.site.register(Transaction)
 
-
+@admin.register(User)
+class UserAdmin(ImportExportModelAdmin):
+    pass
